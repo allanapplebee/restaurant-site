040758ff57688884e552dd8f686fafb629903fe7
Allan Applebee <allan.applebee@gmail.com>
2019-02-21 12:51:51 +1100

    basic server setup. GET request handler added

--- webserver.py
+++ webserver.py
@@ -1,14 +1,32 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_headers('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>Hello</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
+        except IOError:
+            self.send_error(404, ("File Not Found {}").format(self.path))
+
 def main():
     try:
         port = 8080
         server = HTTPServer(('', port), webserverHandler)
         print ("Server is running on port {}").format(port)
         server.serve_forever()
-        
-    except KeyboardInterrupt:
 
+    except KeyboardInterrupt:
+        print "Keyboard interupt, server stopped"
+        server.socket.close()
 
 if __name__ = '__main__':
     main()
