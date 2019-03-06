1e5fa6a87359d80fa4cc16d9cbdb2ea6b0ef94d8
Allan Applebee <allan.applebee@gmail.com>
2019-03-06 13:14:00 +1100

    DB connected. HelloWorld func changed to display menu items of 1st restaurant

--- project.py
+++ project.py
@@ -1,10 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+#DB setup
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return "Hello World"
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += ('<p>{}</p>').format(i.name)
+    return output
 
 if __name__ == '__main__':
     app.debug = True
