67938414968dde02062a50c54d2c290ea3c942f1
Allan Applebee <allan.applebee@gmail.com>
2019-03-07 08:13:01 +1100

    new menu item form added. newMenuItem route updated to handle get and post requests

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -23,9 +23,18 @@ def restaurantMenu(restaurant_id):
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
