0fab38157e5855ed6476a7e1ce0c7c8697db443d
Allan Applebee <allan.applebee@gmail.com>
2019-03-07 07:04:01 +1100

    route declarations for create, edit and delete added

--- project.py
+++ project.py
@@ -16,7 +16,7 @@ Base.metadata.bind = engine
 def restaurantMenu(restaurant_id):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
-    
+
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ''
@@ -27,6 +27,24 @@ def restaurantMenu(restaurant_id):
         output += ('{} </br>').format(i.description)
         output += '</p>'
     return output
+
+# Task 1: Create route for newMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
